af83fbfac0af7f92a844e52075a8f006f4718fb2
RoA10 <o223t029@st.kyoto-seika.ac.jp>
2026-04-19 16:36:37 +0900

    fix

--- middleware.ts
+++ middleware.ts
@@ -1,29 +1,11 @@
 import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
 
-export function middleware(request: NextRequest) {
-  if (request.nextUrl.pathname.startsWith("/admin")) {
-    const basicAuth = request.headers.get("authorization");
-
-    if (basicAuth) {
-      const authValue = basicAuth.split(" ")[1];
-      const [user, pwd] = atob(authValue).split(":");
-
-      if (
-        user === process.env.ADMIN_USER &&
-        pwd === process.env.ADMIN_PASSWORD
-      ) {
-        return NextResponse.next();
-      }
-    }
-
-    return new NextResponse("Authentication required", {
-      status: 401,
-      headers: {
-        "WWW-Authenticate": 'Basic realm="Secure Area"',
-      },
-    });
-  }
-
+export function middleware() {
+  // 何もせず、すべてのアクセスを許可して通す
   return NextResponse.next();
 }
+
+// マッチャーを空にするか、静的ファイルを除外する設定にする
+export const config = {
+  matcher: [],
+};
